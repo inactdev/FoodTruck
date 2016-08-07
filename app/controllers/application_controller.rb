e135d2c3c3fe75921c9e0d539b2b25a2dc90ec0c
Ari Perez <aris.a.perez@gmail.com>
2016-08-07 17:13:53 -0600

    Set up auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  include SessionsHelper
 end
