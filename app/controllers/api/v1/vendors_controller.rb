96f25689c4ad856bb1b34da07ff7ed94eb921d54
Ari Perez <aris.a.perez@gmail.com>
2016-08-09 19:53:41 -0600

    Add search to Vendor controller

--- app/controllers/api/v1/vendors_controller.rb
+++ app/controllers/api/v1/vendors_controller.rb
@@ -1,6 +1,7 @@
 module Api
   module V1
     class VendorsController < ApplicationController
+      before_action :set_search, only: [:index]
       skip_before_filter :verify_authenticity_token
 
       def create
@@ -30,7 +31,9 @@ module Api
       end
 
       def index
-        @vendors = ::Vendor.all
+        # TODO make this more adaptable.  Right now it's set at 5 miles from my house
+        @vendors = ::Vendor.within(5, :origin => [40.386386, -111.881778])
+        search if search_params
 
         render :json => @vendors, :adapter => :json
       end
@@ -58,6 +61,24 @@ module Api
       end
 
       private
+
+      def search
+        @vendors = @vendors.by_name(search_params[:name]) if search_params[:name]
+        @vendors = @vendors.by_food_type(search_params[:food_type]) if search_params[:food_type]
+        # TODO need to make description a fuzzy search
+        @vendors = @vendors.by_description(search_params[:description]) if search_params[:description]
+      end
+
+      def set_search
+        return unless search_params
+        @distance = search_params[:distance]
+        @origin_location = [search_params[:latitude], search_params[:longitude]]
+        @origin_location = ::Vendor.geocode(search_params[:address]) if search_params[:address]
+      end
+
+      def search_params
+        params.require(:search).permit(:name, :food_type, :description, :address, :latitude, :longitude, :distance) if params[:search]
+      end
 
       def vendor_params
         params.require(:vendor).permit(:name, :food_type, :description)
